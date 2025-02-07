6e7379077a1869a3b821959f0e5e528ae0834848
Maab3li <maab3li555@gmail.com>
2025-02-06 21:36:17 -0800

    ,

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import ContactMeSection from "./components/ContactMeSection";
 import Footer from "./components/Footer";
 import { AlertProvider } from "./context/alertContext";
 import Alert from "./components/Alert";
-import { BrowserRouter } from "react-router-dom";
 import { HashRouter } from "react-router-dom";
 
 function App() {
