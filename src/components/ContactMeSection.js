efc339fb47c07b6d1ec926c26304d63d6d646068
Maab3li <maab3li555@gmail.com>
2025-02-13 00:34:03 -0800

    /

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useRef} from "react";
 import { useForm } from "react-hook-form";
+import emailjs from '@emailjs/browser';
 import {
   Box,
   Button,
@@ -19,7 +20,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 const LandingSection = () => {
-  const {register,reset, handleSubmit, formState:{errors,isSubmitSuccessful,isSubmitting, isValid}} = useForm({
+
+  const {register,reset, setValue, handleSubmit, formState:{errors,isSubmitSuccessful,isSubmitting, isValid}} = useForm({
     mode: 'onChange', reValidateMode: 'onChange' ,
     defaultValues: {
       firstName:'',
@@ -29,6 +31,9 @@ const LandingSection = () => {
     }
   })
 
+  const [isSuccess, setIsSuccess] = React.useState(false);
+  const [Message, setMessage] = React.useState("");
+
   const [isSubmitted,setIsSubmitted] = useState(false)
   const {isLoading, response, submit} = useSubmit();
   const { onOpen } = useAlertContext();
@@ -36,6 +41,11 @@ const LandingSection = () => {
   function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
   }
+
+  let name = document.getElementById('firstName')
+  useEffect(() => {
+    setValue('subject', `a new person sent a message from Website`)
+  }, [ setValue]);
  
   const onSubmit = async(data,e) => {
     setIsSubmitted(true)
@@ -43,6 +53,26 @@ const LandingSection = () => {
     e.preventDefault()
     setIsSubmitted(false)
     console.log('data',data)
+    await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data, null, 2),
+    })
+      .then(async (response) => {
+        let json = await response.json();
+        if (json.success) {
+          setIsSuccess(true);
+          setMessage(json.message);
+          e.target.reset();
+          reset();
+        } else {
+          setIsSuccess(false);
+          setMessage(json.message);
+        }
+      })
     onOpen('success','Your info was sent, thanks for your contact.')
   }
 
@@ -71,10 +101,32 @@ const LandingSection = () => {
         </Heading>
         <Box p={6} rounded="md" w="100%" >
           <form 
+          action="https://api.web3forms.com/submit" method="POST"
           onSubmit={handleSubmit(onSubmit,onerror)}
           >
             <VStack spacing={4}>
+            <input
+              type="hidden"
+              value="5a211f9f-ff10-47ab-bdb4-7c62268b5b1f"
+              {...register("access_key")}
+            />
+            <input
+              type="hidden"
+              {...register("subject")}
+            />
+            <input
+              type="hidden"
+              value="Mission Control"
+              {...register("from_name")}
+            />
+            <input
+              type="checkbox"
+              id=""
+              className="hidden"
+              style={{ display: "none" }}
+              {...register("botcheck")}></input>
               <FormControl>
+              <input type="hidden" name="access_key" value="5a211f9f-ff10-47ab-bdb4-7c62268b5b1f" />
                 <FormLabel htmlFor="firstName" color='#28282B' _dark={{color:'#FFF'}}>Name</FormLabel>
                 <Input {...register('firstName', {required: 'Required'})}
                   id="firstName"
@@ -109,10 +161,12 @@ const LandingSection = () => {
                   height={250}
                 />
                 {errors.comment ? <div style={{color:'rgb(250, 41, 41)'}}>{errors.comment?.message}</div> :null}
+                <input type="hidden" name="redirect" value="https://web3forms.com/success" />
               </FormControl>
               <Button type="submit" colorScheme="pink" width="full">
                 {isSubmitted? <FontAwesomeIcon icon={faSpinner} /> :'Submit'}
               </Button>
+              <input type="hidden" name="apikey" value="YOUR_ACCESS_KEY_HERE"></input>
             </VStack>
           </form>
         </Box>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { animate, AnimatePresence, motion } from "framer-motion"
 import '../App.css'
 import DarkModeSwitch from "./DarkModeSwitch";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,8 +13,10 @@ import { HamburgerIcon, MenuIcon } from "@chakra-ui/icons";
 import { Box, HStack, VStack,
   MenuItem, 
   IconButton,
-  CloseButton} from "@chakra-ui/react";
+  CloseButton,
+  transition} from "@chakra-ui/react";
 import { icon } from "@fortawesome/fontawesome-svg-core";
+import { easeInOut } from "motion";
 
 const socials = [
   {
@@ -24,12 +27,12 @@ const socials = [
   {
     name:'LinkedIn',
     icon: faLinkedin,
-    url: "www.linkedin.com/in/maab-ali-46838b302",
+    url: "https://linkedin.com/in/maab-ali-46838b302",
   },
   {
     name:'Github',
     icon: faGithub, 
-    url: "https://github.com",
+    url: "https://github.com/Maab3li",
   }
 ];
 
@@ -37,8 +40,9 @@ const Header = () => {
 
   const [y, setY] = useState(0)
 
-
   const [calculatedY, setCalculatedY] = useState('-200px')
+
+  const [mobileMenuOpened, setMobileMenuOpened] = useState(false)
 
 
   const handleClick = (anchor) => () => {
@@ -54,6 +58,11 @@ const Header = () => {
         block: "start",
       });
     }
+    const mobileMenu = document.getElementById('mobile-menu')
+    if (mobileMenu) {
+      toggleMenu()
+    }
+    else { return }
   };
 
   useEffect(() => {
@@ -70,13 +79,29 @@ const Header = () => {
       setCalculatedY('-200px')
     }
   }
- 
-  function openMenu() {
-    document.getElementById('mobile-menu').style.display ='block'
+
+  function toggleMenu() {
+    setMobileMenuOpened(!mobileMenuOpened)
   }
 
-  function closeMenu() {
-    document.getElementById('mobile-menu').style.display ='none'
+  const menuVars = {
+    initial : {
+      scaleY: 0
+    },
+    animate : {
+      scaleY: 1,
+      transition: {
+        duration: 0.3,
+        easeInOut: [.01,0,0.39,0]
+      }
+    },
+    exit : {
+      scaleY: 0,
+      transition: {
+        duration: 0.3,
+        easeInOut: [0.12,0,0.39,1]
+      }
+    }
   }
 
   return (
@@ -107,13 +132,13 @@ const Header = () => {
           <HStack id="menu" direction='row' spacing={12}>
             <Box display={{base:"none",md:'flex'}}>
             <Box color='blackAlpha.600' _dark={{color:'gray.300'}} px={4}>
-              <a href={socials[0]['url']} ><FontAwesomeIcon icon= {socials[0]['icon']}/></a>
+              <a href={socials[0]['url']} target="_blank"><FontAwesomeIcon icon= {socials[0]['icon']}/></a>
             </Box>
             <Box color='blackAlpha.600' _dark={{color:'gray.300'}} px={4}>
-              <a href={socials[1]['url']} ><FontAwesomeIcon icon= {socials[1]['icon']} /></a>
+              <a href={socials[1]['url']} target="_blank"><FontAwesomeIcon icon= {socials[1]['icon']} /></a>
             </Box>
             <Box color='blackAlpha.600' _dark={{color:'gray.300'}} px={4}>
-              <a href={socials[2]['url']} ><FontAwesomeIcon icon= {socials[2]['icon']}/></a>
+              <a href={socials[2]['url']} target="_blank"><FontAwesomeIcon icon= {socials[2]['icon']}/></a>
             </Box>
             </Box>
           </HStack>
@@ -126,51 +151,60 @@ const Header = () => {
               </Box>
             </HStack>
             </HStack>
-            
-            
+            <AnimatePresence>
+          {mobileMenuOpened && (
+           <motion.div
+           variants ={menuVars}
+            initial = 'initial'
+           animate = 'animate'
+           exit = 'exit'
+           className="origin-top">
             <VStack
-             id="mobile-menu"
-            color="gray.300"
-            spacing={12} 
-            pt={6} 
-            alignContent='center' 
-            alignItems='center' 
-            textAlign='center' 
-            px={4} 
-            display={{base:"flex",md:'none'}}>
-                <CloseButton
-                onClick={closeMenu}
-                ml='auto'
+            id="mobile-menu"
+           color="gray.700"
+           _dark={{color:'gray.300'}}
+           spacing={20}  
+           alignContent='center' 
+           alignItems='center' 
+           textAlign='center' 
+           px={6}
+           height='100vh'
+           display={{base:"block",md:"none"}}
+            >
+              <CloseButton
+              onClick={toggleMenu}
+              ml='70%'
+              className="absolute"
                 >
                 </CloseButton>
-              <Box py={2}>
+              <Box py={6}>
                 <NavLink className="sections" onClick={handleClick('projects')}>Projects</NavLink>
               </Box>
-              <Box py={2}>
+              <Box py={6}>
                 <NavLink className ="sections" onClick={handleClick('contactme')}>Contact Me</NavLink>
               </Box>
 
-            <Box py={2} px={4}>
+            <Box py={6} px={4}>
               <Box>
-              <a href={socials[0]['url']} >{socials[0]['name']}</a>
+              <a href={socials[0]['url']} target="_blank">{socials[0]['name']}</a>
               </Box>
               <Box>
               <FontAwesomeIcon icon= {socials[0]['icon']} style={{paddingLeft:10}}/>
               </Box>
             </Box>
 
-            <Box py={2} px={4}>
+            <Box py={6} px={4}>
               <Box>
-              <a href={socials[1]['url']} >{socials[1]['name']}</a>
+              <a href={socials[1]['url']} target="_blank">{socials[1]['name']}</a>
               </Box>
               <Box>
               <FontAwesomeIcon icon= {socials[1]['icon']} style={{paddingLeft:10}} />
               </Box>
             </Box>
 
-            <Box py={2} px={4} pb={2}>
+            <Box py={6} px={4} pb={2}>
               <Box>
-              <a href={socials[2]['url']} >{socials[2]['name']}</a>
+              <a href={socials[2]['url']} target="_blank">{socials[2]['name']}</a>
               </Box>
               <Box>
               <FontAwesomeIcon icon= {socials[2]['icon']} style={{paddingLeft:10}}/>
@@ -178,12 +212,16 @@ const Header = () => {
               
             </Box>
             </VStack>
-
+            </motion.div>
+          )
+             }
+             </AnimatePresence>
                 <IconButton
-                onClick={openMenu}
+                onClick={toggleMenu}
                 display={{base:"flex",md:'none'}}
                 pos='fixed'
                 top='1rem'
+                color='gray.900'
                 right='4rem'
                 size='sm'
                 p={4}
@@ -192,7 +230,6 @@ const Header = () => {
                 icon={<HamburgerIcon />}
                  />
             <DarkModeSwitch />
-
       </Box>
     </Box>
   );
