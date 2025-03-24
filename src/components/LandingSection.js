c9dab682f8382fdf75900dfb96277e0cdbef332b
Maab3li <maab3li555@gmail.com>
2025-03-23 23:08:17 -0700

    changed avatar image

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,7 +2,7 @@ import React from "react";
 import '../App.css'
 import { Avatar, Heading, HStack, VStack, Wrap, WrapItem, Box, background } from "@chakra-ui/react";
 import { faReact } from "@fortawesome/free-brands-svg-icons";
-import avatar from '../images/avatar.jpg'
+import avatar from '../images/Pi7_cropper (1).png'
 import FullScreenSection from "./FullScreenSection";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -23,7 +23,7 @@ const LandingSection = () => (
   >
     <VStack gap={10} mt={{base:250,xl:280}} >
     <Avatar size='xl'>
-      <img src={avatar} alt="avatar" style={{borderRadius:'50%'}}/>
+      <img src={avatar} alt="avatar" style={{borderRadius:'70%', backgroundColor:'gray'}} />
     </Avatar>
     <Box fontSize='xl' color='#28282B' _dark={{color:'white'}} fontWeight={600}>
       {greeting}
