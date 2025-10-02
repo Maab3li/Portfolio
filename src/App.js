5ec97a45f9c999287717c97eff38daaac1eb6c8c
Maab3li <maab3li555@gmail.com>
2025-10-02 16:03:47 -0700

    added basename property to browser router / added base to public/index.html file

--- src/App.js
+++ src/App.js
@@ -7,12 +7,11 @@ import ContactMeSection from "./components/ContactMeSection";
 import Footer from "./components/Footer";
 import { AlertProvider } from "./context/alertContext";
 import Alert from "./components/Alert";
-import { HashRouter } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Portfolio">
     <ChakraProvider>
       <AlertProvider>
         <main>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import '../App.css'
 import DarkModeSwitch from "./DarkModeSwitch";
